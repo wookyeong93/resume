d8f4ed5a0e464b76c3b0ea4348f2896845e18a8c
wooykeong93 <dnrud368@gmail.com>
2026-06-25 23:46:28 +0900

    work폴더 임시삭제

--- app/work/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function WorkPage(){
-    return (
-        <main className="p-10">
-            <h1 className="font-display text-4xl text-ink">프로젝트 목록</h1>
-        </main>
-    )
-}
